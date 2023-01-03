ee2b456f11a4cdf0fd1cd05bbefdb1dd069b1bd4
kamkhno <kostiantynmakhno@gmail.com>
2023-01-03 18:11:43 +0200

    SD: Add empty sd_simple.v file
    Signed-off-by: kamkhno <kostiantynmakhno@gmail.com>

---
+++ src/sd_simple.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01/03/2023 06:10:43 PM
+// Design Name: 
+// Module Name: sd_simple
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module sd_simple(
+
+    );
+endmodule
