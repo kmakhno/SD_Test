08e7b6970af545926826bb415beb7940f6480cc9
kmakhno <kostiantynmakhno@gmail.com>
2023-01-03 20:57:48 +0200

    SD: Add simple SD state machine
    Signed-off-by: kmakhno <kostiantynmakhno@gmail.com>

--- src/sd_simple.v
+++ src/sd_simple.v
@@ -32,10 +32,17 @@ module sd_simple
     inout DAT3_io
 );
 
+localparam reg [2:0] SD_INIT = 3'h0,
+                     SD_IDLE = 3'h1,
+                     SD_READ = 3'h2,
+                     SD_WRITE = 3'h3;
+
 parameter PRESCALER = 400;
 
 reg [$clog2(PRESCALER/2)-1:0] fod_cnt_q;
 reg fod_clk_q;
+
+reg [2:0] sd_state_q;
 
 always @(posedge clk_i) begin
     if (rst_i) begin
@@ -47,6 +54,30 @@ always @(posedge clk_i) begin
             fod_cnt_q <= 0;
             fod_clk_q <= ~fod_clk_q;
         end
+    end
+end
+
+always @(posedge clk_i) begin
+    if (rst_i) begin
+        sd_state_q <= INIT;
+    end else begin
+        case (sd_state_q)
+            INIT: begin
+            end
+
+            IDLE: begin
+            end
+
+            READ: begin
+            end
+
+            WRITE: begin
+            end
+
+            default: begin
+                sd_state_q <= IDLE;
+            end
+        endcase
     end
 end
 
