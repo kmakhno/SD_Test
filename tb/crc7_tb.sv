2e93f6396b3e34bacfcb1e53badc06f8b8dfb3bb
kmakhno <kostiantynmakhno@gmail.com>
2023-01-03 22:29:49 +0200

    CRC7: Add simple test for crc module
    Signed-off-by: kmakhno <kostiantynmakhno@gmail.com>

--- tb/crc7_tb.sv
+++ tb/crc7_tb.sv
@@ -43,7 +43,30 @@ initial begin
 end
 
 initial begin
-    
+    en_i = 0;
+    data_i = 0;
+    #350;
+    @(posedge clk_i);
+
+    //Stimulus from Physical Layer Specification Version 3.01 (p.65)
+    en_i <= 1'b1;
+    data_i <= 40'h4000000000; //CMD0
+    @(posedge clk_i);
+    en_i <= 1'b0;
+    repeat (42) @(posedge clk_i);
+
+    en_i <= 1'b1;
+    data_i <= 40'h5100000000; //CMD17
+    @(posedge clk_i);
+    en_i <= 1'b0;
+    repeat (42) @(posedge clk_i);
+
+    en_i <= 1'b1;
+    data_i <= 40'b0001000100000000000000000000100100000000; //response CMD17
+    @(posedge clk_i);
+    en_i <= 1'b0;
+    repeat (42) @(posedge clk_i);
+    $finish;
 end
 
 endmodule
