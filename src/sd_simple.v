3b6f3a763aac619cb5fb9b2b489696007a662a7f
kmakhno <kostiantynmakhno@gmail.com>
2023-01-03 20:50:44 +0200

    Clock: Implement 400kHz clock generator
    Signed-off-by: kmakhno <kostiantynmakhno@gmail.com>

--- src/sd_simple.v
+++ src/sd_simple.v
@@ -32,5 +32,24 @@ module sd_simple
     inout DAT3_io
 );
 
+parameter PRESCALER = 400;
+
+reg [$clog2(PRESCALER/2)-1:0] fod_cnt_q;
+reg fod_clk_q;
+
+always @(posedge clk_i) begin
+    if (rst_i) begin
+        fod_cnt_q <= 0;
+        fod_clk_q <= 1'b0;
+    end else begin
+        fod_cnt_q <= fod_cnt_q + 1'b1;
+        if (fod_cnt_q == (PRESCALER/2)-1) begin
+            fod_cnt_q <= 0;
+            fod_clk_q <= ~fod_clk_q;
+        end
+    end
+end
+
+
 
 endmodule
